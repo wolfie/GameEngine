3667d0a86f5af72e18a2f59160268517488b3aa1
Henrik Paul <henrik.paul@gmail.com>
2012-06-29 19:13:51 +0300

    made mouse clicks more accurate (now take margin into account)

--- src/com/github/wolfie/engine/MouseData.java
+++ src/com/github/wolfie/engine/MouseData.java
@@ -14,9 +14,14 @@ public class MouseData implements MouseMotionListener, MouseListener {
 	public static final int MIDDLE_MOUSE = 1;
 	public static final int RIGHT_MOUSE = 2;
 
+	private static final int MARGIN_Y = 5;
+	private static final int MARGIN_X = 5;
+
 	private static final boolean[] FALSE_ARRAY = new boolean[3];
 	public boolean[] mouseButtonWasPressed = new boolean[3];
 	public boolean[] mouseButtonIsPressed = new boolean[3];
+	public boolean[] mouseButtonWasDragged = new boolean[3];
+	public boolean[] mouseButtonIsDragged = new boolean[3];
 	public int x = -1;
 	public int y = -1;
 	public int prevX;
@@ -35,15 +40,14 @@ public class MouseData implements MouseMotionListener, MouseListener {
 	public void mousePressed(final MouseEvent e) {
 		final int clickedButton = e.getButton();
 		if (clickedButton >= 1 && clickedButton <= 3) {
-			mouseButtonWasPressed[clickedButton - 1] = true;
 			mouseButtonIsPressed[clickedButton - 1] = true;
 		}
 		updateCoordinates(e);
 	}
 
 	private void updateCoordinates(final MouseEvent e) {
-		x = e.getX() / EngineCanvas.SCALE;
-		y = e.getY() / EngineCanvas.SCALE;
+		x = (e.getX() - MARGIN_X) / EngineCanvas.SCALE;
+		y = (e.getY() - MARGIN_Y) / EngineCanvas.SCALE;
 	}
 
 	@Override
@@ -51,6 +55,7 @@ public class MouseData implements MouseMotionListener, MouseListener {
 		final int clickedButton = e.getButton();
 		if (clickedButton >= 1 && clickedButton <= 3) {
 			mouseButtonIsPressed[clickedButton - 1] = false;
+			mouseButtonIsDragged[clickedButton - 1] = false;
 		}
 		fixConfinedCursorIfNeeded(e);
 		updateCoordinates(e);
@@ -87,6 +92,9 @@ public class MouseData implements MouseMotionListener, MouseListener {
 	@Override
 	public void mouseDragged(final MouseEvent e) {
 		updateCoordinates(e);
+		for (int i = 0; i < mouseButtonIsPressed.length; i++) {
+			mouseButtonIsDragged[i] = mouseButtonIsPressed[i];
+		}
 	}
 
 	@Override
@@ -102,13 +110,11 @@ public class MouseData implements MouseMotionListener, MouseListener {
 	}
 
 	public void tick() {
-		Arrays.fill(mouseButtonWasPressed, false);
+		System.arraycopy(mouseButtonIsPressed, 0, mouseButtonWasPressed, 0,
+				mouseButtonIsPressed.length);
+		System.arraycopy(mouseButtonIsDragged, 0, mouseButtonWasDragged, 0,
+				mouseButtonIsDragged.length);
 		prevX = x;
 		prevY = y;
-	}
-
-	public boolean isBeingDragged(final int mouseButton) {
-		return mouseButtonIsPressed[mouseButton]
-				&& !mouseButtonWasPressed[mouseButton];
 	}
 }
